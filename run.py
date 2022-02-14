0e5d83810ec19ece300af213a4399def5a587be2
justkirst <kirsty.mcgregor@me.com>
2022-02-14 16:15:53 +0000

    Import random for computer turns in game

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# Functions to build the board and let the player choose their playing piece
+
+def players_board():
